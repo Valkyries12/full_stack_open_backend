949145e141eb4a753337557dd74c02dac9368c10
ncaruso <nico.caruso1992@outlook.com>
2026-05-06 08:40:24 -0300

    ui build deploy

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(cors());
 app.use(express.json());
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(express.static("dist"));
 
 let persons = [
     { 
@@ -105,7 +106,7 @@ app.post("/api/persons", (request, response) => {
 
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 });
